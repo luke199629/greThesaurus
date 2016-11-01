6b846e1515238ca82690139041c064c6f4539209
Ke Lu <luke199629@gmail.com>
2016-10-31 22:26:34 -0500

    Changed layout

---
+++ greThesaurus/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  greThesaurus
+//
+//  Created by Ke Lu on 10/31/16.
+//  Copyright © 2016 kelu2zhao102. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MainViewController:UIViewController {
+    
+    @IBOutlet weak var cardView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cardView.backgroundColor = .green
+    }
+}
